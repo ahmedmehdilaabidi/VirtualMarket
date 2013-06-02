37825ccbc94777984b01c2fd60e36f213b83e8fb
ahmed <ahmed@ahmed-Aspire-5738>
2013-06-02 10:54:21 +0100

    commit diagramm

--- com.esprit.miniprojet.virtualMarket/src/main/java/com/esprit/market/domain/User.java
+++ com.esprit.miniprojet.virtualMarket/src/main/java/com/esprit/market/domain/User.java
@@ -1,6 +1,17 @@
 package com.esprit.market.domain;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class User {
+	
+	
+	private String id;
+	private String username;
+	private String password;
 
 	public User() {
 
@@ -9,6 +20,26 @@ public class User {
 	public void authentication(String login, String password) {
 		System.out.println("test");
      
+	}
+
+	
+	
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
 	}
 
 }
