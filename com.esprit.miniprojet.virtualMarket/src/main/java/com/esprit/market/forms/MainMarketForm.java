ce9a85bd968babd1b48d114957b3e10a5cf98bc8
ahmed <ahmed@ahmed-Aspire-5738>
2013-06-02 14:25:59 +0100

    commit swt

---
+++ com.esprit.miniprojet.virtualMarket/src/main/java/com/esprit/market/forms/MainMarketForm.java
@@ -0,0 +1,54 @@
+package com.esprit.market.forms;
+
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Shell;
+import org.eclipse.swt.widgets.Group;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.custom.ScrolledComposite;
+
+public class MainMarketForm {
+
+	protected Shell shell;
+
+	/**
+	 * Launch the application.
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		try {
+			MainMarketForm window = new MainMarketForm();
+			window.open();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * Open the window.
+	 */
+	public void open() {
+		Display display = Display.getDefault();
+		createContents();
+		shell.open();
+		shell.layout();
+		while (!shell.isDisposed()) {
+			if (!display.readAndDispatch()) {
+				display.sleep();
+			}
+		}
+	}
+
+	/**
+	 * Create contents of the window.
+	 */
+	protected void createContents() {
+		shell = new Shell();
+		shell.setSize(450, 300);
+		shell.setText("SWT Application");
+		
+		Group grpListOfProducts = new Group(shell, SWT.NONE);
+		grpListOfProducts.setText("List of Products");
+		grpListOfProducts.setBounds(10, 10, 411, 239);
+
+	}
+}
--- com.esprit.miniprojet.virtualMarket/src/test/java/com/esprit/market/Test/SWTHelloWorld.java
+++ com.esprit.miniprojet.virtualMarket/src/test/java/com/esprit/market/Test/SWTHelloWorld.java
@@ -4,22 +4,34 @@ import org.eclipse.swt.SWT;
 import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.Shell;
 import org.eclipse.swt.widgets.Text;
+
+import com.esprit.market.forms.AuthenForm;
+import com.esprit.market.forms.MainMarketForm;
  
 public class SWTHelloWorld {
  
 public static void main (String [] args) {
+	
 	Display display = new Display ();
 	Shell shell = new Shell(display);
+	
+//	AuthenForm a=new AuthenForm(shell,shell.getStyle());
+//	a.open();
+	
+	MainMarketForm m=new MainMarketForm();
+	m.open();
+	
+	
+	
+	
+	
+	
+	
  
-	Text helloWorldTest = new Text(shell, SWT.NONE);
-	helloWorldTest.setText("Hello World SWT");
-	helloWorldTest.pack();
- 
-	shell.pack();
-	shell.open ();
-	while (!shell.isDisposed ()) {
-		if (!display.readAndDispatch ()) display.sleep ();
-	}
-	display.dispose ();
+	
+	
+	
+
+
 }
 }
