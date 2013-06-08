12603d3d9ff023b8e17fd6ba9b398ba55800158b
ahmed <ahmed@ahmed-Aspire-5738>
2013-06-08 09:41:31 +0100

    commit stock

---
+++ com.esprit.miniprojet.virtualMarket/src/main/java/com/esprit/market/domain/Stock.java
@@ -0,0 +1,21 @@
+package com.esprit.market.domain;
+
+import java.io.Serializable;
+import javax.persistence.*;
+
+/**
+ * Entity implementation class for Entity: Stock
+ *
+ */
+@Entity
+
+public class Stock implements Serializable {
+
+	
+	private static final long serialVersionUID = 1L;
+
+	public Stock() {
+		super();
+	}
+   
+}
